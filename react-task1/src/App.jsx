e9bc6ca9e72cb9882d13d782a07f58ef9aed208f
Dmitry Zharikov <dimbltoss@gmail.com>
2025-02-13 21:40:56 +0300

    rewrote App.jsx without using jsx code

--- react-task1/src/App.jsx
+++ react-task1/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { createElement, Fragment, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,35 +7,63 @@ export const App = () => {
 	const [count, setCount] = useState(0);
 	const currentYear = new Date().getFullYear();
 
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React 2</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			{currentYear}
-		</>
+	return createElement(
+		Fragment,
+		{},
+		createElement(
+			"div",
+			{},
+			createElement(
+				"a",
+				{
+					href: "https://vite.dev",
+					target: "_blank",
+				},
+				createElement("img", {
+					src: viteLogo,
+					className: "logo",
+					alt: "Vite logo",
+				})
+			),
+			createElement(
+				"a",
+				{
+					href: "https://react.dev",
+					target: "_blank",
+				},
+				createElement("img", {
+					src: reactLogo,
+					className: "logo react",
+					alt: "React logo",
+				})
+			)
+		),
+		createElement("h1", {}, "Vite + React"),
+		createElement(
+			"div",
+			{ className: "card" },
+			createElement(
+				"button",
+				{
+					onClick: () => {
+						setCount((count) => count + 1);
+					},
+				},
+				`count is ${count}`
+			),
+			createElement(
+				"p",
+				{},
+				"Edit ",
+				createElement("code", {}, "src/App.jsx"),
+				" and save to test HMR"
+			)
+		),
+		createElement(
+			"p",
+			{ className: "read-the-docs" },
+			"Click on the Vite and React logos to learn more"
+		),
+		currentYear
 	);
 };
-
-// Здесь все декларативный стиль
